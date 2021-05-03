9737f4791fbdbeba70257e8fca3137dfcb05e938
Ivan <ivan@shoplineapp.com>
2021-05-03 18:22:12 +0800

    feat: remove online fetch, use current branch instead

--- script.sh
+++ script.sh
@@ -10,12 +10,7 @@ fi
 
 git config http.${BITBUCKET_GIT_HTTP_ORIGIN}.proxy http://host.docker.internal:29418/
 
-git fetch --unshallow
-git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
-git fetch origin
-latest_version=$(git branch -r | grep 'release/' | cut -d '/' -f3 | sort -t. -k 1,1nr -k 2,2nr -k 3,3nr | head -n 1)
-git checkout release/"$latest_version"
-
+origin_branch=$(git rev-parse --abbrev-ref HEAD)
 branch=feature/${GEM_NAME}-version-${TAG}
 git checkout -b "${branch}"
 
